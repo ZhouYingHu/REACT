73652a6cb3fd9acbe394be011f8195cf01805928
周瀛 <2922034995@qq.com>
2022-08-17 17:47:17 +0800

    父子和ref通信

---
+++ src/01-base/09-父子表单域.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+class Field extends Component {
+    state = {
+        value: ''
+    }
+    clear() {
+        this.setState({
+            value: ''
+        })
+    }
+    render() {
+        return <div>
+            <label htmlFor={this.props.name}>{this.props.name}</label>
+            <input id={this.props.name} type={this.props.type} value={this.state.value} onChange={
+                (event) => {
+                    this.setState({
+                        value: event.target.value
+                    })
+                }
+            } ></input>
+        </div>
+    }
+}
+export default class App extends Component {
+    username = React.createRef()
+    password = React.createRef()
+    render() {
+        return <div>
+            <Field type='text' name="姓名" ref={this.username}></Field>
+            <Field type="password" name="密码" ref={this.password} ></Field>
+            <button onClick={() => {
+                console.log(this.username.current.state.value)
+                console.log(this.password.current.state.value)
+            }}>登录</button>
+            <button onClick={() => {
+                this.username.current.clear()
+                this.password.current.clear()
+            }}>取消</button>
+
+        </div>
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line
 import React from "react"
 import ReactDOM from "react-dom"
-import App from "./01-base/08-受控卖座选项卡"
+import App from "./01-base/09-父子表单域"
 ReactDOM.render(<App> </App>, document.getElementById("root"))
