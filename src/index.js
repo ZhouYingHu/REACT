35f1976894371ad77bb81ef76cfac8439277f5ea
周瀛 <2922034995@qq.com>
2023-06-09 10:40:56 +0800

    1231

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 // eslint-disable-next-line
 import React from "react"
-import ReactDOM from 'react-dom';
-import App from './03-router/App'
-ReactDOM.render(<App />, document.getElementById('root'));
+// import ReactDOM from 'react-dom';
+import App from './04-test/test.js'
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
